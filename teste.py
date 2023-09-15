a142bd01367a208d3624ecd83909b719ff0942c0
leosfreitas <leonardosf@al.insper.edu.br>
2023-09-15 16:29:25 -0300

    Commit pro kike

---
+++ teste.py
@@ -0,0 +1,8 @@
+# Lista original com palavras repetidas
+lista = ["eu", "eu", "eu", "joao", "joao"]
+
+# Converter a lista para um conjunto para remover duplicatas e, em seguida, convertê-lo de volta para uma lista
+lista_sem_duplicatas = list(set(lista))
+
+# Imprimir a lista resultante
+print(lista_sem_duplicatas)
