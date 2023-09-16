ad360bd6a0b47fbfd4344f75e346abb4230263df
leosfreitas <leonardosf@al.insper.edu.br>
2023-09-16 18:37:50 -0300

    final

--- teste.py
+++
@@ -1,8 +0,0 @@
-# Lista original com palavras repetidas
-lista = ["eu", "eu", "eu", "joao", "joao"]
-
-# Converter a lista para um conjunto para remover duplicatas e, em seguida, convertê-lo de volta para uma lista
-lista_sem_duplicatas = list(set(lista))
-
-# Imprimir a lista resultante
-print(lista_sem_duplicatas)
